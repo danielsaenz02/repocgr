241ef7ab4c94c4bfda4a41b7fc3c87280ce1b6e0
Daniel S Saenz <Daniel.Saenz@co.ey.com>
2023-02-16 10:49:40 -0500

    first commit

---
+++ src/main/java/com/cgr/cgrApp/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cgr.cgrApp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CgrAppApplication.class);
+	}
+
+}
